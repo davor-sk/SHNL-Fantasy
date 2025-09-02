bb68d7c13f591f662b764e3248d6d5f9f400054d
Davor <davor.skiljic@gmail.com>
2025-09-02 23:07:50 +0200

    Dodana komponenta za dodane igrace + provjere je li igrac vec dodan i je li dodano 3 igraca iz istog kluba

--- src/components/Igraci.vue
+++ src/components/Igraci.vue
@@ -2,9 +2,11 @@
 import { ref, onMounted, computed } from "vue";
 import { db } from "@/firebase";
 import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import MojaEkipa from "./MojaEkipa.vue";
 
 const props = defineProps(["klub"]);
 const igraci = ref([]);
+const odabraniIgraci = ref([{}]);
 
 async function loadPlayers() {
   const q = query(collection(db, "players"), orderBy("Pozicija"));
@@ -34,39 +36,80 @@ console.log(igraci);
 const filtriraniIgraci = computed(() =>
   igraci.value.filter((i) => i.klubId === props.klub)
 );
+
+const klubBrojac = {
+  dinamo: 0,
+  hajduk: 0,
+  rijeka: 0,
+  iIstra: 0,
+  lokomotiva: 0,
+  gorica: 0,
+  vukovar: 0,
+  osijek: 0,
+  slaven: 0,
+  varaždin: 0,
+};
+function dodajIgraca(igrac) {
+  const { klubId } = igrac;
+
+  if (klubId in klubBrojac) {
+    if (klubBrojac[klubId] > 2) return;
+    klubBrojac[klubId]++;
+    console.log(`${klubId} =`, klubBrojac[klubId]);
+  }
+  if (klubBrojac[klubId] > 3) return;
+
+  const postoji = odabraniIgraci.value.find((i) => i.id === igrac.id);
+  if (postoji) return;
+  odabraniIgraci.value.push(igrac);
+}
 </script>
 
 <template>
   <div class="h-full flex flex-col justify-center items-center gap-2 mt-2 p-4">
-    <div v-if="props.klub" class="flex flex-col gap-2">
-      <ol class="border bg-gray-100 p-4 rounded-lg text-gray-700">
-        <li v-for="i in filtriraniIgraci" :key="i.id">
-          <span v-if="i.pozicija == 'GK'" @click="console.log(i.ime)"
-            >{{ i.ime }} {{ i.pozicija }} {{ i.cijena }}M €</span
-          >
-        </li>
-      </ol>
-      <ol class="border bg-gray-100 p-4 rounded-lg text-gray-700">
-        <li v-for="i in filtriraniIgraci" :key="i.id">
-          <span v-if="i.pozicija == 'DEF'" @click="console.log(i.ime)"
-            >{{ i.ime }} {{ i.pozicija }} {{ i.cijena }}M €</span
-          >
-        </li>
-      </ol>
-      <ol class="border bg-gray-100 p-4 rounded-lg text-gray-700">
-        <li v-for="i in filtriraniIgraci" :key="i.id">
-          <span v-if="i.pozicija == 'MID'" @click="console.log(i.ime)"
-            >{{ i.ime }} {{ i.pozicija }} {{ i.cijena }}M €</span
-          >
-        </li>
-      </ol>
-      <ol class="border bg-gray-100 p-4 rounded-lg text-gray-700">
-        <li v-for="i in filtriraniIgraci" :key="i.id">
-          <span v-if="i.pozicija == 'FWD'" @click="console.log(i.ime)"
-            >{{ i.ime }} {{ i.pozicija }} {{ i.cijena }}M €</span
-          >
-        </li>
-      </ol>
-    </div>
+    <MojaEkipa :odabrani-igraci="odabraniIgraci">
+      <div v-if="klub" class="flex flex-col gap-2">
+        <ol class="border bg-gray-100 p-4 rounded-lg text-gray-700">
+          <li v-for="i in filtriraniIgraci" :key="i.id">
+            <span
+              v-if="i.pozicija == 'GK'"
+              @click="dodajIgraca(i)"
+              class="cursor-pointer"
+              >{{ i.ime }} {{ i.pozicija }} {{ i.cijena }}M €</span
+            >
+          </li>
+        </ol>
+        <ol class="border bg-gray-100 p-4 rounded-lg text-gray-700">
+          <li v-for="i in filtriraniIgraci" :key="i.id">
+            <span
+              v-if="i.pozicija == 'DEF'"
+              @click="dodajIgraca(i)"
+              class="cursor-pointer"
+              >{{ i.ime }} {{ i.pozicija }} {{ i.cijena }}M €</span
+            >
+          </li>
+        </ol>
+        <ol class="border bg-gray-100 p-4 rounded-lg text-gray-700">
+          <li v-for="i in filtriraniIgraci" :key="i.id">
+            <span
+              v-if="i.pozicija == 'MID'"
+              @click="dodajIgraca(i)"
+              class="cursor-pointer"
+              >{{ i.ime }} {{ i.pozicija }} {{ i.cijena }}M €</span
+            >
+          </li>
+        </ol>
+        <ol class="border bg-gray-100 p-4 rounded-lg text-gray-700">
+          <li v-for="i in filtriraniIgraci" :key="i.id">
+            <span
+              v-if="i.pozicija == 'FWD'"
+              @click="dodajIgraca(i)"
+              class="cursor-pointer"
+              >{{ i.ime }} {{ i.pozicija }} {{ i.cijena }}M €</span
+            >
+          </li>
+        </ol>
+      </div>
+    </MojaEkipa>
   </div>
 </template>
---
+++ src/components/MojaEkipa.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, computed } from "vue";
+
+defineProps(["odabraniIgraci"]);
+</script>
+
+<template>
+  <div class="h-full flex justify-center items-center gap-2 mt-2 p-4">
+    <slot />
+    <div class="flex flex-col">
+      <span v-for="i in odabraniIgraci" :key="i.id">
+        {{ i.ime }}
+      </span>
+    </div>
+  </div>
+</template>
